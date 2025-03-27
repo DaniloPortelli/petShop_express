ddcafa6e939829d7a0eea7a7fab0c9cf81f50746
alessiozaccato <alessiozaccato@gmail.com>
2025-03-27 18:57:38 +0100

    FIX: indent app.js

--- app.js
+++ app.js
@@ -2,18 +2,18 @@ import express from 'express';
 import cors from 'cors';
 import 'dotenv/config'
 import imagePathMiddleware from './middlewares/imagePath.js'
-   //impostiamo express e la porta del server
-  const app = express();
-  const port = 3000;
-  import productRuoter from './routes/productRouter.js'
-  
-  app.use(cors());
-  app.use(express.static("public"))
-  app.use(express.json());
-  app.use("/products",productRuoter)
-  app.use(imagePathMiddleware)
+//impostiamo express e la porta del server
+const app = express();
+const port = 3000;
+import productRuoter from './routes/productRouter.js'
 
-   //attivazione del server
-   app.listen(port, () => {
-      console.log(`Server in funzione sulla porta: ${port}`);
-   });
+app.use(cors());
+app.use(express.static("public"))
+app.use(express.json());
+app.use("/products", productRuoter)
+app.use(imagePathMiddleware)
+
+//attivazione del server
+app.listen(port, () => {
+   console.log(`Server in funzione sulla porta: ${port}`);
+});

324e173e5c2cabe864b8e67dfb5f2d26a3beb0b4
DaniloPortelli <daniloportelli@gmail.com>
2025-03-31 17:54:54 +0200

    Add: chiamate API per categorie

--- controllers/productController.js
+++ controllers/productController.js
@@ -11,4 +11,117 @@ function index(req, res) {
         res.json(results);
     });
 }
-export default index
+
+/*Chiamata per cibo per cani*/
+
+function showDogsFood(req, res) {
+    const sql = `
+    SELECT * 
+    FROM products p
+    JOIN categories c ON p.category_id = c.id
+    WHERE c.name = 'Cibo' AND p.animals = 'cani'`;
+
+    connection.query(sql, (err, results) => {
+        if (err)
+            return res.status(500).json({
+                error: 'Errore lato server INDEX function',
+            });
+
+        res.json(results);
+    });
+}
+
+/*Chiamata per giochi per cani*/
+
+function showDogsGames(req, res) {
+    const sql = `
+    SELECT * 
+    FROM products p
+    JOIN categories c ON p.category_id = c.id
+    WHERE c.name = 'Giochi' AND p.animals = 'cani'`;
+
+    connection.query(sql, (err, results) => {
+        if (err)
+            return res.status(500).json({
+                error: 'Errore lato server INDEX function',
+            });
+
+        res.json(results);
+    });
+}
+
+/*Chiamata per cibo per gatti*/
+
+function showCatsFood(req, res) {
+    const sql = `
+    SELECT * 
+    FROM products p
+    JOIN categories c ON p.category_id = c.id
+    WHERE c.name = 'Cibo' AND p.animals = 'gatti'`;
+
+    connection.query(sql, (err, results) => {
+        if (err)
+            return res.status(500).json({
+                error: 'Errore lato server INDEX function',
+            });
+
+        res.json(results);
+    });
+}
+
+/*Chiamata per giochi per gatti*/
+
+function showCatsGames(req, res) {
+    const sql = `
+    SELECT * 
+    FROM products p
+    JOIN categories c ON p.category_id = c.id
+    WHERE c.name = 'Giochi' AND p.animals = 'gatti'`;
+
+    connection.query(sql, (err, results) => {
+        if (err)
+            return res.status(500).json({
+                error: 'Errore lato server INDEX function',
+            });
+
+        res.json(results);
+    });
+}
+
+/*Chiamata per accessori*/
+
+function showAccessories(req, res) {
+    const sql = `
+    SELECT * 
+    FROM products p
+    JOIN categories c ON p.category_id = c.id
+    WHERE c.name = 'Accessori'`;
+
+    connection.query(sql, (err, results) => {
+        if (err)
+            return res.status(500).json({
+                error: 'Errore lato server INDEX function',
+            });
+
+        res.json(results);
+    });
+}
+
+/*Chiamata per prodotti in offerta*/
+
+function showDiscountedProducts(req, res) {
+    const sql = `
+    SELECT * 
+    FROM products 
+    WHERE discounted_price < price`;
+
+    connection.query(sql, (err, results) => {
+        if (err)
+            return res.status(500).json({
+                error: 'Errore lato server INDEX function',
+            });
+
+        res.json(results);
+    });
+}
+export { index, showDogsFood, showDogsGames, showCatsFood, showCatsGames, showAccessories, showDiscountedProducts };
--- routes/productRouter.js
+++ routes/productRouter.js
@@ -2,8 +2,27 @@ import express from 'express'
 
 const router= express.Router()
 
-import  index from '../controllers/productController.js';
+import  {index, showDogsFood, showDogsGames, showCatsFood, showCatsGames, showAccessories, showDiscountedProducts } from '../controllers/productController.js';
 
 router.get('/', index);
+// Rotta per ottenere il cibo per cani
+router.get('/cani/food', showDogsFood);
+
+// Rotta per ottenere i giochi per cani
+router.get('/cani/games', showDogsGames);
+
+// Rotta per ottenere il cibo per gatti
+router.get('/gatti/food', showCatsFood);
+
+// Rotta per ottenere i giochi per gatti
+router.get('/gatti/games', showCatsGames);
+
+// Rotta per ottenere gli accessori
+router.get('/accessories', showAccessories);
+
+// Rotta per ottenere i prodotti sconto
+router.get('/discounted', showDiscountedProducts);
+
+
 
 export default router
